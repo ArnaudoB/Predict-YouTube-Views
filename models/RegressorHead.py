3e0f025c380cf779a8a543d6da16bf2c506294ba
Baptiste Arnaudo <baptiste.arnaudo@polytechnique.edu>
2025-05-05 21:08:09 +0200

    Saved processed dataset, fixed the model

--- models/RegressorHead.py
+++ models/RegressorHead.py
@@ -1,14 +1,28 @@
 import torch.nn as nn
+from constants import n_features
+# n_features = 1873
 
 class RegressorHead(nn.Module):
-    def __init__(self, input_dim, output_dim=1):
+    def __init__(self, input_dim=n_features, output_dim=1, dropout=0.1):
         super(RegressorHead, self).__init__()
         self.regression_head = nn.Sequential(
-            nn.Linear(input_dim, input_dim // 2),
+            nn.Linear(input_dim, input_dim // 2), # 1873 -> 936
             nn.ReLU(),
-            nn.Linear(input_dim // 2, input_dim // 4),
+            nn.Dropout(dropout),
+
+            nn.Linear(input_dim // 2, input_dim // 4), # 936 -> 468
             nn.ReLU(),
-            nn.Linear(input_dim // 4, output_dim)
+            nn.Dropout(dropout),
+
+            nn.Linear(input_dim // 4, input_dim // 8), # 468 -> 234
+            nn.ReLU(),
+            nn.Dropout(dropout),
+
+            nn.Linear(input_dim // 8, input_dim//16), # 234 -> 117
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            
+            nn.Linear(input_dim // 16, output_dim) # 117 -> 1
         )
 
     def forward(self, x):
---
+++ models/constants.py
@@ -0,0 +1,4 @@
+n_features_tabular = 31
+clip_dim = 512
+bert_dim = 768
+n_features = 2*clip_dim + bert_dim + n_features_tabular # = 1873
