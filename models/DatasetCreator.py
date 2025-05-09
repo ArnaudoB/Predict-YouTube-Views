674d3d6fc96b91b7f6472fa11e20f6954ab09ba3
Rayane <128234596+RayaneChikhi@users.noreply.github.com>
2025-05-09 15:28:28 +0200

    Add files via upload

---
+++ models/DatasetCreator.py
@@ -0,0 +1,31 @@
+import torch
+import numpy as np
+from PIL import Image
+import clip
+import pandas as pd
+from tqdm import tqdm
+
+class Dataset():
+    def __init__(self, set = "training_set.csv"):
+        self.set = set
+        self.data = self.create_dataset()
+    
+    def create_dataset(self):
+        _, preprocess = clip.load("ViT-B/32", device="cuda" if torch.cuda.is_available() else "cpu")
+        df = pd.read_csv(self.set, sep=";")
+        tabular_columns = [col for col in df.columns if col not in ['title', 'description', 'id', 'logviews']]
+        
+        dataset = []
+        for idx, row in tqdm(df.iterrows(), total=len(df)):
+            image = Image.open("train_val/"+row['id']+".jpg").convert("RGB")
+            sample = {
+                "image": preprocess(image),
+                "title": row['title'],
+                "description": row['description'],
+                "tabular": torch.tensor(row[tabular_columns].values.astype(np.float32)),
+                "target": torch.tensor(row["logviews"], dtype=torch.float32)
+            }
+            dataset.append(sample)
+           
+        return dataset
+
