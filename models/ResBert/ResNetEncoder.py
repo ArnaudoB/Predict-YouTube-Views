b80e865ed4796d29338fa128b722d447cdd36ff9
Baptiste Arnaudo <baptiste.arnaudo@polytechnique.edu>
2025-05-18 19:58:59 +0200

    New model and modified EDA

---
+++ models/ResBert/ResNetEncoder.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+from torchvision import models
+
+class ResNetEncoder(nn.Module):
+    def __init__(self, output_dim=256, dropout=0.3, frozen=True, semifrozen=False):
+        super(ResNetEncoder, self).__init__()
+        self.resnet = models.resnet50(weights='IMAGENET1K_V2')
+        
+        # Remove the final fully connected layer
+        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+        
+        # Add a global average pooling layer
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.img_dim = 2048  # ResNet50 output dimension after global average pooling
+
+        if frozen:
+            for param in self.resnet.parameters():
+                param.requires_grad = False
+
+        # Unfreeze the last layer of ResNet if semifrozen is True
+        elif semifrozen:
+            for name, param in self.resnet.named_parameters():
+                if name.startswith('7.'):
+                    param.requires_grad = True
+                    print(f"Unfreezing {name}")
+                else:
+                    param.requires_grad = False
+                    print(f"Freezing {name}")
+        
+        # Projection layer to reduce dimensions
+        self.projection_model = nn.Sequential(nn.Linear(self.img_dim, output_dim),
+                                                  nn.ReLU(),
+                                                  nn.LayerNorm(output_dim),
+                                                  nn.Dropout(dropout))
+
+        
+    def forward(self, x):
+        # Pass the input through the ResNet model
+        x = self.resnet(x)
+        
+        # Apply global average pooling
+        x = self.global_avg_pool(x)
+        
+        # Flatten the output
+        x = x.view(x.size(0), -1)
+
+        # Pass through the projection model
+        x = self.projection_model(x)
+        
+        return x
